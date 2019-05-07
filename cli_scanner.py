e57fd83a9f562092635c7580b916b23d2c1e1b35
mkbeh <mkbehforever@gmail.com>
2019-05-07 21:28:50 +0300

    Added cli interface.

---
+++ cli_scanner.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import argparse
+import asyncio
+import uvloop
+
+from pyshella_scanner.scanner import scanner
+
+
+cli_desc = 'Scanner which parse Bitcoin or forks peers and writes them into file.'
+uri_help_text = 'Node URI.'
+ban_time_help_text = 'The time which will be banned each peer.'
+interval_help_text = 'Interval between call cycles for new peers.'
+epilog = """
+
+|-----------------|
+|Created by @mkbeh|
+|-----------------|
+"""
+
+
+def _run_scanner(**kwargs):
+    ioloop = asyncio.get_event_loop()
+    ioloop.run_until_complete(scanner(kwargs))
+
+
+def cli():
+    parser = argparse.ArgumentParser(prog='pyshella_scanner', formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description=cli_desc, epilog=epilog)
+    parser.add_argument('-u', '--uri', nargs=1, required=True, metavar='', dest='uri', type=str, help=uri_help_text)
+    parser.add_argument('-b', '--ban-time', nargs=1, metavar='', default=14 * 86400, type=int, help=ban_time_help_text)
+    parser.add_argument('-i', '--interval', nargs=1, metavar='', default=60, type=int, help=interval_help_text)
+
+    args = parser.parse_args()
+    _run_scanner(uri=args.uri[0], ban_time=args.ban_time, interval=args.interval)
+
+
+if __name__ == '__main__':
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    cli()
--- pyshella_scanner/scanner.py
+++ pyshella_scanner/scanner.py
@@ -2,18 +2,13 @@
 # TODO
 #   1. добавить логгирование при добавлении новых пиров и количество.
 #   3. создать отдельную папку в директории пользователя и туда класть файлы и логи
-#   4. добавить cli интерфейс
 import time
-import asyncio
-import uvloop
+import logging
+
 
 from aiobitcoin.grambitcoin import GramBitcoin
+from aiobitcoin.bitcoinerrors import NoConnectionToTheDaemon
 from extra import decorators
-
-
-url = 'http://bitcoinrpc:80d3fa20b89f12225a4d3d54634601c7@46.160.199.52:18332'
-ban_time = 14 * 86400
-interval = 60
 
 
 async def _get_ip_from_addr(addr):
@@ -21,8 +16,8 @@ async def _get_ip_from_addr(addr):
 
 
 @decorators.log('peers.lst')
-async def _add_new_peers():
-    async with GramBitcoin(url=url) as gram:
+async def _add_new_peers(uri, ban_time):
+    async with GramBitcoin(url=uri) as gram:
         for count, peer_info in enumerate(await gram.get_peer_info(to_list=False)):
             if count < 2:
                 continue
@@ -33,18 +28,13 @@ async def _add_new_peers():
             yield f'{addr}:{subver}\n'
 
 
-async def scanner():
+async def scanner(args):
+    uri, ban_time, interval = args.values()
+
     while True:
-        await _add_new_peers()
-        # time.sleep(interval)
-        break
-
-
-def main():
-    ioloop = asyncio.get_event_loop()
-    ioloop.run_until_complete(scanner())
-
-
-if __name__ == '__main__':
-    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-    main()
+        try:
+            await _add_new_peers(uri, ban_time)
+        except NoConnectionToTheDaemon:
+            logging.warning('Сonnection to daemon was lost.')
+        finally:
+            time.sleep(interval)
