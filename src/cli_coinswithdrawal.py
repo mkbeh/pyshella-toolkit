2686d9fd88a838b4e56568723329ac2e0a3c4162
mkbeh <mkbehforever@gmail.com>
2019-06-07 14:04:02 +0300

    Added skeleton for 'coins-withdrawal' and its cli.

--- src/cli_coinswithdrawal.py
+++ src/cli_coinswithdrawal.py
@@ -1,1 +1,41 @@
 # -*- coding-utf-8 -*-
+import argparse
+import asyncio
+import uvloop
+
+from src.toolkit.coinswithdrawal import CoinsWithdrawal
+
+
+cli_description = 'Utility which withdrawal crypto currency from bruted JSON-RPC.'
+cli_interval_help = 'Timeout after coins withdrawal from all the peers that were collected in the database ' \
+                    'at the moment.'
+cli_epilog = """
+-----------------------------------------------------------------------------------
+Usage example: [TO DO]
+
+|-----------------|
+|Created by @mkbeh|
+|-----------------|
+"""
+
+
+def _run_coins_withdrawal(**kwargs):
+    uvloop.install()
+    asyncio.run(
+        CoinsWithdrawal(**kwargs).coins_withdrawal()
+    )
+
+
+def cli():
+    parser = argparse.ArgumentParser(prog='pyshella-coins-withdrawal', description=cli_description, epilog=cli_epilog,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-n', '--coin-name', required=True, metavar=' ', type=str, help='Name of cryptocurrency.')
+    parser.add_argument('-mU', '--mongo-uri', required=True, metavar=' ', type=str, help='MongoDB uri.')
+    parser.add_argument('-i', '--interval', metavar='', default=3*60, type=int, help=cli_interval_help)
+
+    from secret import args_lst4
+    _run_coins_withdrawal(**vars(parser.parse_args(args_lst4)))
+
+
+if __name__ == '__main__':
+    cli()
--- src/toolkit/coinswithdrawal.py
+++ src/toolkit/coinswithdrawal.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+from aiobitcoin.wallet import Wallet
+from src.extra.pymongodb import PyMongoDB
+
+
+class CoinsWithdrawal:
+    def __init__(self, **kwargs):
+        self._coin_name = kwargs.get('coin_name')
+        self._interval = kwargs.get('interval')
+
+        self.mongo_withdrawal = PyMongoDB(db_name='withdrawal', uri=kwargs.get('mongo_uri'))
+
+    async def coins_withdrawal(self):
+        pass
