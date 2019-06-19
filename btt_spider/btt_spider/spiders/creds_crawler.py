b7b91461a77690077617fcec75d99a88e611c026
mkbeh <mkbehforever@gmail.com>
2019-06-19 15:16:59 +0300

    Updated `creds crawler` (added skeleton of searching creds for each topic in page)

--- btt_spider/btt_spider/spiders/creds_crawler.py
+++ btt_spider/btt_spider/spiders/creds_crawler.py
@@ -13,26 +13,28 @@ class CredsCrawler(scrapy.Spider):
         f'https://bitcointalk.org/index.php?board=159.{_page_num}'
     ]
 
+    def search_creds(self, response):
+        pass
+
+    def get_topic_urls(self, response):
+        required_table_num = 8 if self._iter_count == 0 else 7
+        self._iter_count += 1
+        href_xpath = 'td[position() = 3]/span/a/@href'
+
+        return response.xpath('//table')[required_table_num].xpath('tr')[1:].xpath(href_xpath).getall()
+
     def _set_last_page_num(self, response):
         self._last_page_num = \
             int(response.xpath('//table')[9].xpath('//a/@href')[-13].extract().split('.')[-1])
-
-    def _parse_topics(self, response, table_num):
-        href_xpath = 'td[position() = 3]/span/a/@href'
-
-        for topic_url in response.xpath('//table')[table_num].xpath('tr')[1:].xpath(href_xpath):
-            print(topic_url.extract())
-
-        self._page_num += 40
-        self._iter_count += 1
 
     def parse(self, response):
         if not self._last_page_num:
             self._set_last_page_num(response)
 
-        required_table_num = 8 if self._iter_count == 0 else 7
-        self._parse_topics(response, required_table_num)
-        next_page = response.urljoin(self._next_page_url_pattern.format(self._page_num))
+        for url in self.get_topic_urls(response):
+            yield scrapy.Request(url, callback=self.search_creds)
 
-        if self._last_page_num >= self._page_num:
-            yield scrapy.Request(next_page, callback=self.parse)
+        self._page_num += 40
+
+        if self._page_num <= self._last_page_num:
+            yield scrapy.Request(self._next_page_url_pattern.format(self._page_num), callback=self.parse)
